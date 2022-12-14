584206914ada9e4fc7895ae8484559465b48be2a
manzanita0412 <36706403+manzanita0412@users.noreply.github.com>
2022-12-14 01:37:15 +0000

    Fix prior commit

--- custom_components/ocpp/api.py
+++ custom_components/ocpp/api.py
@@ -584,6 +584,7 @@ class ChargePoint(cp):
                 cs_charging_profiles={
                     om.charging_profile_id.value: 8,
                     om.stack_level.value: stack_level,
+                    om.start_schedule.value: "2013-01-01T00:00Z",
                     om.charging_profile_kind.value: ChargingProfileKindType.absolute.value,
                     om.charging_profile_purpose.value: ChargingProfilePurposeType.charge_point_max_profile.value,
                     om.charging_schedule.value: {
